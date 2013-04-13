b54d477e907acd448577c2905d2fb03768ac35fe
Pascal Comte <pascal.comte@univ-paris1.fr>
2013-04-13 21:06:24 +0000

    Modif suite
    git-svn-id: https://subversion.renater.fr/g2t/G2T-v3@9 d2c9f5c4-e840-43a4-9a69-01eb24ae821f

--- CRON/cron.bat
+++ CRON/cron.bat
@@ -1,10 +1,10 @@
 del .\log\trace_cron.log
 del .\log\trace_cron_error.log
 
-REM php import_agent.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
-REM php import_absence.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
-REM php import_structure.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
-REM php import_affectation.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
+php import_agent.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
+php import_absence.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
+php import_structure.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
+php import_affectation.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
 php calcul_solde.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
 
 REM php artt_maj_solde_rtt_deb_periode.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
