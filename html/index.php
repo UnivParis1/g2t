5194a81d45dd9a7ab5459eb9c5391bcb88171210
Elodie Briere <Elodie.Briere@univ-paris1.fr>
2016-02-15 15:32:31 +0000

    GLPI #0054362 Amélioration de l'affichage mobile
    git-svn-id: https://subversion.renater.fr/g2t/G2T-v3@127 d2c9f5c4-e840-43a4-9a69-01eb24ae821f

--- html/index.php
+++ html/index.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr"> 
 <head> 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<meta name="viewport" content="width=device-width, initial-scale=0.8" />
 <?php
 	require_once('CAS.php');
 	include './includes/casconnection.php';
