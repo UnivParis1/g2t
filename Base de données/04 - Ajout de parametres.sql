ca3ea8de09b4b2e7aeb89e6fbf12d1fb933ae259
Pascal Comte <pascal.comte@univ-paris1.fr>
2017-03-16 14:39:48 +0000

    [IMP] : Permet de mapper le personnel d'une structure sur une autre
    git-svn-id: https://subversion.renater.fr/g2t/G2T-v3@158 d2c9f5c4-e840-43a4-9a69-01eb24ae821f

---
+++ Base de données/04 - Ajout de parametres.sql
@@ -0,0 +1,4 @@
+INSERT INTO `CONSTANTES` (`NOM`, `VALEUR`) VALUES ('LDAPMEMBERATTR', 'memberof');
+INSERT INTO `CONSTANTES` (`NOM`, `VALEUR`) VALUES ('LDAPGROUPNAME', 'cn=applications.g2t.users,ou=groups,dc=univ-paris1,dc=fr');
+INSERT INTO `CONSTANTES` (`NOM`, `VALEUR`) VALUES ('WSGROUPURL', 'https://wsgroups.univ-paris1.fr/');
+
