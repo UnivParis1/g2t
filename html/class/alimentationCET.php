01f1809251ecae9a852c5ef8c0273e5f81210904
Pascal Comte <pascal.comte@univ-paris1.fr>
2021-03-30 15:44:21 +0200

    [NEW] Nouvelle classe pour l'alimentation du CET

---
+++ html/class/alimentationCET.php
@@ -0,0 +1,456 @@
+<?php
+class alimentationCET
+{
+    
+    private $dbconnect = null;
+    private $fonctions = null;
+    
+    private $alimentationid = null;
+    private $harpegeid = null;
+    private $datecreation = null;
+    private $esignatureid = null;
+    private $esignatureurl = null;
+    private $valeur_a = null;
+    private $valeur_b = null;
+    private $valeur_c = null;
+    private $valeur_d = null;
+    private $valeur_e = null;
+    private $valeur_f = null;
+    private $valeur_g = null;
+    private $statut = null;
+    private $datestatut = null;
+    
+    
+    
+    function __construct($db)
+    {
+        $this->dbconnect = $db;
+        if (is_null($this->dbconnect)) {
+            $errlog = "alimentationCET->construct : La connexion à la base de donnée est NULL !!!";
+            echo $errlog . "<br/>";
+            error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+        }
+        $this->fonctions = new fonctions($db);
+    }
+    
+    function alimentationid()
+    {
+        return $this->alimentationid;
+    }
+    
+    function agentid($agentid = null)
+    {
+        if (is_null($agentid)) {
+            if (is_null($this->harpegeid)) {
+                $errlog = "alimentationCET->agentid : Le numéro agent n'est pas défini !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            } else
+                return $this->harpegeid;
+        } 
+        else
+        {
+            if (!is_null($this->harpegeid))
+            {
+                $errlog = "alimentationCET->agentid : Impossible de modifier le numéro de l'agent !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            }
+            else
+                $this->harpegeid = $agentid;
+        }
+    }
+    
+    function datecreation()
+    {
+        if (!is_null($this->datecreation))
+        {
+            $info = explode(' ',$this->datecreation);
+            //echo "info = " . print_r($info, true) . "<br>";
+            return $this->fonctions->formatdate($info[0]) . " " . $info[1];
+        }  
+        else
+            return $this->datecreation;
+    }
+    
+    function esignatureid($esignatureid = null)
+    {
+        if (is_null($esignatureid)) {
+            if (is_null($this->esignatureid)) {
+                $errlog = "alimentationCET->esignatureid : L'identifiant eSignature n'est pas défini !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            } else
+                return $this->esignatureid;
+        }
+        else
+        {
+            if ($this->esignatureid . '' != '')
+            {
+                $errlog = "alimentationCET->esignatureid : Impossible de modifier lidentifiant eSignature !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            }
+            else
+                $this->esignatureid = $esignatureid;
+        }
+    }
+    
+    function esignatureurl($esignatureurl = null)
+    {
+        if (is_null($esignatureurl)) {
+            if (is_null($this->esignatureurl)) {
+                $errlog = "alimentationCET->esignatureurl : L'URL eSignature n'est pas défini !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            } else
+                return $this->esignatureurl;
+        }
+        else
+        {
+            if ($this->esignatureurl . '' != '')
+            {
+                $errlog = "alimentationCET->esignatureurl : Impossible de modifier l'URL eSignature !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            }
+            else
+                $this->esignatureurl = $esignatureurl;
+        }
+    }
+     
+    function valeur_a($valeur_a = null)
+    {
+        if (is_null($valeur_a)) {
+            if (is_null($this->valeur_a)) {
+                $errlog = "alimentationCET->valeur_a : La valeur de la case A n'est pas définie !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            } else
+                return $this->valeur_a;
+        }
+        else
+        {
+            if (!is_null($this->valeur_a))
+            {
+                $errlog = "alimentationCET->esignatureurl : Impossible de modifier la valeur de la case A !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            }
+            else
+                $this->valeur_a = $valeur_a;
+        }
+    }
+    
+    function valeur_b($valeur_b = null)
+    {
+        if (is_null($valeur_b)) {
+            if (is_null($this->valeur_b)) {
+                $errlog = "alimentationCET->valeur_b : La valeur de la case B n'est pas définie !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            } else
+                return $this->valeur_b;
+        }
+        else
+        {
+            if (!is_null($this->valeur_b))
+            {
+                $errlog = "alimentationCET->valeur_b : Impossible de modifier la valeur de la case B !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            }
+            else
+                $this->valeur_b = $valeur_b;
+        }
+    }
+    
+    function valeur_c($valeur_c = null)
+    {
+        if (is_null($valeur_c)) {
+            if (is_null($this->valeur_c)) {
+                $errlog = "alimentationCET->valeur_c : La valeur de la case C n'est pas définie !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            } else
+                return $this->valeur_c;
+        }
+        else
+        {
+            if (!is_null($this->valeur_c))
+            {
+                $errlog = "alimentationCET->valeur_c : Impossible de modifier la valeur de la case C !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            }
+            else
+                $this->valeur_c = $valeur_c;
+        }
+    }
+    
+    function valeur_d($valeur_d = null)
+    {
+        if (is_null($valeur_d)) {
+            if (is_null($this->valeur_d)) {
+                $errlog = "alimentationCET->valeur_d : La valeur de la case D n'est pas définie !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            } else
+                return $this->valeur_d;
+        }
+        else
+        {
+            if (!is_null($this->valeur_d))
+            {
+                $errlog = "alimentationCET->valeur_d : Impossible de modifier la valeur de la case D !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            }
+            else
+                $this->valeur_d = $valeur_d;
+        }
+    }
+    
+    function valeur_e($valeur_e = null)
+    {
+        if (is_null($valeur_e)) {
+            if (is_null($this->valeur_e)) {
+                $errlog = "alimentationCET->valeur_e : La valeur de la case E n'est pas définie !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            } else
+                return $this->valeur_e;
+        }
+        else
+        {
+            if (!is_null($this->valeur_e))
+            {
+                $errlog = "alimentationCET->valeur_e : Impossible de modifier la valeur de la case E !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            }
+            else
+                $this->valeur_e = $valeur_e;
+        }
+    }
+    
+    function valeur_f($valeur_f = null)
+    {
+        if (is_null($valeur_f)) {
+            if (is_null($this->valeur_f)) {
+                $errlog = "alimentationCET->valeur_f : La valeur de la case F n'est pas définie !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            } else
+                return $this->valeur_f;
+        }
+        else
+        {
+            if (!is_null($this->valeur_f))
+            {
+                $errlog = "alimentationCET->valeur_f : Impossible de modifier la valeur de la case F !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            }
+            else
+                $this->valeur_f = $valeur_f;
+        }
+    }
+    
+    function valeur_g($valeur_g = null)
+    {
+        if (is_null($valeur_g)) {
+            if (is_null($this->valeur_g)) {
+                $errlog = "alimentationCET->valeur_g : La valeur de la case G n'est pas définie !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            } else
+                return $this->valeur_g;
+        }
+        else
+        {
+            if (!is_null($this->valeur_g))
+            {
+                $errlog = "alimentationCET->valeur_g : Impossible de modifier la valeur de la case G !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            }
+            else
+                $this->valeur_g = $valeur_g;
+        }
+    }
+    
+    function statut($statut = null)
+    {
+        if (is_null($statut)) {
+            if (is_null($this->statut)) {
+                $errlog = "alimentationCET->statut : La valeur du statut n'est pas défini !!!";
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            } else
+                return $this->statut;
+        }
+        else
+        {
+            if ($this->statut <> $statut)
+            {
+                $this->statut = $statut;
+                $this->datestatut = $this->fonctions->formatdatedb(date("d/m/Y"));
+            }
+        }
+    }
+    
+    function datestatut()
+    {
+        return $this->datestatut;
+    }
+    
+    
+    function load($esignatureid = null, $alimentationid = null, $agentid = null )
+    {
+        $errlog = '';
+        $sql = "SELECT ALIMENTATIONID,HARPEGEID,DATECREATION,ESIGNATUREID,ESIGNATUREURL,VALEUR_A,VALEUR_B,VALEUR_C,VALEUR_D,VALEUR_E,VALEUR_F,VALEUR_G,STATUT,DATESTATUT FROM ALIMENTATIONCET WHERE ";
+        if (!is_null($esignatureid))
+        {
+            $sql = $sql . "ESIGNATUREID = '$esignatureid'";
+        }
+        elseif (!is_null($alimentationid))
+        {
+            $sql = $sql . "ALIMENTATIONID = '$alimentationid'";
+        }
+        elseif (!is_null($agentid))
+        {
+            $sql = $sql . "HARPEGEID = '$agentid' ORDER BY DATECREATION DESC, ALIMENTATIONID ASC LIMIT 1";
+        }
+        else
+        {
+            $errlog = "alimentationCET->Load : Tous les paramètres sont vides !!!";
+            echo $errlog . "<br/>";
+            error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            return $errlog;
+        }
+        
+        $query = mysqli_query($this->dbconnect, $sql);
+        $erreur = mysqli_error($this->dbconnect);
+        if ($erreur != "")
+        {
+            $errlog = "alimentationCET->Load : " . $erreur;
+            echo $errlog . "<br/>";
+            error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            return $errlog;
+        }
+        
+        if (mysqli_num_rows($query) == 0)
+        {
+            //echo "<br>load => pas de ligne dans la base de données<br>";
+            $errlog = "alimentationCET->Load : Aucune ligne dans la base de données correspondante";
+            error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            return $errlog;
+        }
+        
+        $result = mysqli_fetch_row($query);
+        $this->alimentationid   = "$result[0]";
+        $this->harpegeid        = "$result[1]";
+        $this->datecreation     = "$result[2]";
+        $this->esignatureid     = "$result[3]";
+        $this->esignatureurl    = "$result[4]";
+        $this->valeur_a         = "$result[5]";
+        $this->valeur_b         = "$result[6]";
+        $this->valeur_c         = "$result[7]";
+        $this->valeur_d         = "$result[8]";
+        $this->valeur_e         = "$result[9]";
+        $this->valeur_f         = "$result[10]";
+        $this->valeur_g         = "$result[11]";
+        $this->statut           = "$result[12]";
+        $this->datestatut       = "$result[13]";
+        
+        return $errlog;
+    }
+
+    function store()
+    {
+        $erreur = '';
+        // Si on est en train de créer une demande d'alimentation
+        if (is_null($this->alimentationid))
+        {
+            //echo "alimentationCET->Store : Création d'une nouvelle alimentation <br>";
+            // On doit vérifier que les éléments olbigatoires sont bien renseignés : HarpegeId, valeur_a, valeur_b, valeur_c, valeur_d, valeur_e,, valeur_f, valeur_g
+            if (is_null($this->harpegeid) 
+             or is_null($this->valeur_a) 
+             or is_null($this->valeur_b) 
+             or is_null($this->valeur_c) 
+             or is_null($this->valeur_d) 
+             or is_null($this->valeur_e) 
+             or is_null($this->valeur_f)
+             or is_null($this->valeur_g))
+            {
+                // Au moins un des éléments obligatoires est null => pas de sauvegarde possible
+                $erreur = "Au moins un des éléments obligatoires est null => Pas de création possible";
+                $errlog = "alimentationCET->Store : " . $erreur;
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+                return $erreur;
+            }
+
+            $this->datecreation = $this->fonctions->formatdatedb(date("d/m/Y"));
+            $this->datestatut  = $this->datecreation;
+            $this->statut = "ENC";
+            $this->esignatureid = '';
+            $this->esignatureurl = '';
+            $sql = "LOCK TABLES ALIMENTATIONCET WRITE";
+            mysqli_query($this->dbconnect, $sql);
+            $sql = "SET AUTOCOMMIT = 0";
+            mysqli_query($this->dbconnect, $sql);
+            $sql = "INSERT INTO ALIMENTATIONCET(HARPEGEID,DATECREATION,ESIGNATUREID,ESIGNATUREURL,VALEUR_A,VALEUR_B,VALEUR_C,VALEUR_D,VALEUR_E,VALEUR_F,VALEUR_G,STATUT,DATESTATUT) 
+                    VALUES('". $this->harpegeid . "',
+                           now(),
+                           '" . $this->esignatureid . "',
+                           '" . $this->esignatureurl . "',
+                           '" . $this->valeur_a . "',
+                           '" . $this->valeur_b . "',
+                           '" . $this->valeur_c . "',
+                           '" . $this->valeur_d . "',
+                           '" . $this->valeur_e . "',
+                           '" . $this->valeur_f . "',
+                           '" . $this->valeur_g . "',
+                           '" . $this->statut . "',
+                           '" . $this->datestatut . "')";
+             echo "SQL = " . $sql . "<br>";
+            $query = mysqli_query($this->dbconnect, $sql);
+            $erreur = mysqli_error($this->dbconnect);
+            if ($erreur != "") {
+                $errlog = "alimentationCET->Store (INSERT) : " . $erreur;
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            }
+            $this->alimentationid = mysqli_insert_id($this->dbconnect);
+            $sql = "COMMIT";
+            mysqli_query($this->dbconnect, $sql);
+            $sql = "UNLOCK TABLES";
+            mysqli_query($this->dbconnect, $sql);
+            $sql = "SET AUTOCOMMIT = 1";
+            mysqli_query($this->dbconnect, $sql);
+        }
+        // Sinon on modifie un enregistrement
+        else
+        {
+            //echo "alimentationCET->Store : Mise à jour d'une nouvelle alimentation <br>";
+            $sql = "UPDATE ALIMENTATIONCET 
+                    SET ESIGNATUREID = '" . $this->esignatureid . "',
+                        ESIGNATUREURL = '" . $this->esignatureurl . "',
+                        STATUT = '" . $this->statut . "',
+                        DATESTATUT = '" . $this->datestatut . "'
+                    WHERE ALIMENTATIONID = '" . $this->alimentationid . "'";
+            //echo "SQL alimentationCET->Store : $sql <br>";
+            $query = mysqli_query($this->dbconnect, $sql);
+            $erreur = mysqli_error($this->dbconnect);
+            if ($erreur != "") {
+                $errlog = "alimentationCET->Store (UPDATE) : " . $erreur;
+                echo $errlog . "<br/>";
+                error_log(basename(__FILE__) . " " . $this->fonctions->stripAccents($errlog));
+            }
+        }
+        return $erreur;
+    }
+    
+}
+?>
