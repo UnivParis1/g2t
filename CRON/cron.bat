05e305bc92f1594288589ed8d5127dfe7efd34c7
Pascal Comte <pascal.comte@univ-paris1.fr>
2021-04-12 11:19:35 +0200

    [IMP] Modif appel PHP

--- CRON/cron.bat
+++ CRON/cron.bat
@@ -1,19 +1,28 @@
-REM set PHPDIR=d:\php
-set PHPDIR=C:\wamp64\bin\php\php7.3.21
-
+@ECHO OFF 
 del .\log\trace_cron.log
 del .\log\trace_cron_error.log
 
+for /f "delims=" %%a in (' powershell "Split-Path -Path (Get-Item C:\wamp64\bin\apache\apache2.4.46\bin\php.ini).Target" ') do set "PHPDIR=%%a"
+@ECHO PHPDIR = %PHPDIR% >>./log/trace_cron.log
+
+if not defined PHPDIR (
+   echo PHPDIR is NOT defined >>./log/trace_cron.log
+   exit /B
+)
+
+REM set PHPDIR=d:\php
+REM set PHPDIR=C:\wamp64\bin\php\php7.3.21
+REM set PHPDIR=C:\wamp64\bin\php\php7.4.9
+
+%PHPDIR%\php --version >> ./log/trace_cron.log
+
 REM %PHPDIR%\php import_agent.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
 REM %PHPDIR%\php import_absence.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
-REM %PHPDIR%\php import_structure.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
+%PHPDIR%\php import_structure.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
 REM %PHPDIR%\php p1_specific_update.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
-v%PHPDIR%\php import_affectation_siham.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
+REM %PHPDIR%\php import_affectation_siham.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
 REM %PHPDIR%\php p1_post_affectation.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
 REM %PHPDIR%\php calcul_solde.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
-
-
-
 
 REM ----------------------------------------
 REM -- SCRIPT DE MIGRATION V2 - V3
@@ -22,5 +31,5 @@ REM %PHPDIR%\php migration_v2_v3.php -d error_log=./log/PHP_Log.log >>./log/trac
 REM %PHPDIR%\php mail_alerte_reliquats.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
 REM %PHPDIR%\php mail_conges.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
 REM %PHPDIR%\php mail_declarationTP.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
-%PHPDIR%\php generer_solde.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
+REM %PHPDIR%\php generer_solde.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
 REM %PHPDIR%\php demande_cet.php -d error_log=./log/PHP_Log.log >>./log/trace_cron.log 2>>./log/trace_cron_error.log
