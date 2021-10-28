e43105f13e666c5a78d2f2b03bbcfb954ef13e80
Pascal Comte <pascal.comte@univ-paris1.fr>
2021-10-28 14:53:04 +0200

    [IMP] Ajout des balises de début et fin de script

--- CRON/synchro_demandes_cet.php
+++ CRON/synchro_demandes_cet.php
@@ -3,10 +3,13 @@
     require_once ('../html/includes/g2t_ws_url.php');
     require_once ('../html/includes/all_g2t_classes.php');
 
+    echo "Début de la synchronisation des demandes Alim + Option CET " . date("d/m/Y H:i:s") . "\n";
     $fonctions = new fonctions($dbcon);    
 
     $anneeref = $fonctions->anneeref();
     $typeconge = $fonctions->typeCongeAlimCET();
 	$fonctions->synchroGlobaleCETeSignature($typeconge, $anneeref);
 
+	echo "Fin de la synchronisation des demandes Alim + Option CET " . date("d/m/Y H:i:s") . "\n";
+	
 ?>
