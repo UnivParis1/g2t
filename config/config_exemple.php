2347f84ecec90d3fcc121de5bea72ebb9ee74b1d
Pascal Comte <pascal.comte@univ-paris1.fr>
2022-01-20 16:35:58 +0100

    [FIX] Fichier d'exemple de config G2T

---
+++ config/config_exemple.php
@@ -0,0 +1,12 @@
+<?php
+
+// URL d'accès aux WS G2T
+define('G2T_WS_URL', "http://host_name:port/webservice_folder");
+
+// Connexion à la base de données
+define('DB_HOST', 'host_name:port');
+define('DB_USER', 'db_user');
+define('DB_PWD', 'db_password');
+define('DB_NAME', 'g2t_db');
+
+?>
